659d7d046bdffe84060c547bb561a4de1beefb1b
Paramesvaran S <param.senthil03@gmail.com>
2026-08-31 12:01:51 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -45,4 +45,4 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-app.listen(5000, () => console.log("ONE LINK APP: http://localhost:5000"));
+app.listen(process.env.PORT || 5000, () => console.log("ONE LINK APP: http://localhost:5000"));
